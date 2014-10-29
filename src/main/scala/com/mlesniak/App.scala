ada25c52eafea355de330e303fafae4e7dc3a222
Michael Lesniak <mail@mlesniak.com>
2014-10-29 21:47:09 +0100

    Example fix.

--- src/main/scala/com/mlesniak/App.scala
+++ src/main/scala/com/mlesniak/App.scala
@@ -1,5 +1,7 @@
 package com.mlesniak
 
+import scala.sys.process._
+
 object App extends App {
   def mul(x: Int): Int = {
     2 * x
@@ -8,4 +10,7 @@ object App extends App {
   println("Hello World! " + mul(10))
   val lj = new LegacyJava
   lj.legacyCall()
+
+  val out = "ls -1".!!
+  println(out)
 }
